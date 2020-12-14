49203f8730eb920fba6333e0dbea34d36a0f9f86
Santiago Villa <santiagovillavega@gmail.com>
2020-12-14 17:56:05 -0300

    Style added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 class App extends Component {
@@ -41,9 +43,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} 
-          delTodo={this.delTodo}
-        />
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos todos={this.state.todos} markComplete={this.markComplete} 
+            delTodo={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
